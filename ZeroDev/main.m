75259b4b959171884bcea81ee0216543bfd66783
yuyang <623240480@qq.com>
2015-11-06 10:25:03 +0800

    Initial Commit

---
+++ ZeroDev/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZeroDev
+//
+//  Created by admin on 15/11/6.
+//  Copyright (c) 2015年 cn.lztech  合肥联正电子科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
